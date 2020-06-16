1b8892d23057cdd96796544c4376942900cfd658
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-16 11:47:43 +0530

    UC-1:FirstName Added

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,3 +2,17 @@
 
 echo "--------WELCOME TO USER REGISTRATION PROCESS---------"
 
+function FirstName()
+{
+	fnamePattern="^[A-Z]([a-z]{2,})$"
+
+	if [[ $1 =~ $fnamePattern ]]
+	then
+		echo "Valid First Name"
+	else
+		echo "Invalid First Name"
+	fi
+}
+
+read -p " First Name : " fname
+FirstName $fname
